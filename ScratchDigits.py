271e37195151701d87c2021b5f7e248f9368cc17
cfarris <connerfarris@gmail.com>
2018-12-07 06:08:49 -0700

    saving

--- ScratchDigits.py
+++ ScratchDigits.py
@@ -5,7 +5,7 @@ from ScratchNN import *
 print("Starting...")
 
 num_classes = 10
-digits_X = load_digits().data
+digits_X = load_digits().data / 16
 digits_y = load_digits().target
 
 np.random.seed(84)
@@ -19,12 +19,12 @@ print("Training...")
 
 # train
 one_hot_targets_train = np.eye(num_classes)[digits_y_train]
-nn = ScratchNN(num_layers=4, num_nodes=[digits_X.shape[1], 100, 50, num_classes], activation_function=[None, "sigmoid", "sigmoid", "softmax"], cost_function="mean_squared")
-nn.train(batch_size=1, inputs=digits_X_train, labels=one_hot_targets_train, num_epochs=100, learning_rate=0.001, filename="irissavepoint.pkl")
+nn = ScratchNN(num_layers=4, num_nodes=[digits_X.shape[1], 500, 250, num_classes], activation_function=[None, "sigmoid", "sigmoid", "softmax"], cost_function="mean_squared")
+nn.train(batch_size=1, inputs=digits_X_train, labels=one_hot_targets_train, num_epochs=100, learning_rate=0.001, filename="digitssavepoint.pkl")
 
 # print("Testing...")
 #
 # # test
 # one_hot_targets_test = np.eye(num_classes)[iris_y_test]
-# nn.check_accuracy("irissavepoint.pkl", iris_X_test, one_hot_targets_test)
+# nn.check_accuracy("digitssavepoint.pkl", iris_X_test, one_hot_targets_test)
 
